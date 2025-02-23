bdaad3b0c87d054e5d5345423fb29e43ee6077f9
SenthilL13 <113251501+SenthilL13@users.noreply.github.com>
2025-02-23 16:53:50 +0530

    ProjectSec.jsx

--- Portfolio/src/Components/ProjectSec.jsx
+++ Portfolio/src/Components/ProjectSec.jsx
@@ -51,7 +51,7 @@ export const ProjectSec = () => {
          <ProjectCardSec
           title="Billing System"
           main="Create a new invoice bill and integrate it into the dashboard screen, allowing users to filter by date to extract the data into an Excel file. The bill should include features such as adding items, calculating totals, applying GST, updating, deleting, and more options for better management."
-          imgSrc="https://ibb.co/Nd2kp92S"
+          imgSrc="/assets/billing.png"
           demoLink="https://billssystem.netlify.app/"
           sourceCodeLink="https://github.com/SenthilL13/Billingsystem"
         />
